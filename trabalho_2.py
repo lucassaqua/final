1fd048ec73c83e81a3159c1d0faf977d6e3cd2e7
lucas <lucassaqua@live.com>
2022-04-17 20:48:36 -0300

    fim

--- trabalho_2.py
+++ trabalho_2.py
@@ -3,7 +3,6 @@
 #%matplotlib qt
 import callback as call  
 import numpy as np
-import matrizes as mt
 import matplotlib.pyplot as plt
 
 # Aqui voce pode optar por visualizar um quadrado ou um triangulo 
@@ -12,10 +11,7 @@ matriz = np.array([[2,2,1], [4,2,1], [3,4,1]]) # triangulo
 
 po = np.transpose(matriz)
 
-xMenor = np.amin(po[0, :])  # xmenor e ymenor nao sao usadas. No nosso do periodo passado era usados no centro_eixo_x
 xMaior = np.amax(po[0, :])
-
-yMenor = np.amin(po[1, :])
 yMaior = np.amax(po[1, :])
 
 centroX = 0.
@@ -29,14 +25,13 @@ while not call.end_loop:
         if call.end_loop == True:
             break
             
-        # se tu souber o que significa esse prd renomeia ele 
-        prd = np.matmul( call.matriz_translacao(call.centroX, call.centroY), call.matriz_escala(call.escala))
+        transformacao = np.matmul( call.matriz_translacao(call.centroX, call.centroY), call.matriz_escala(call.escala))
         
         thetaRadianos = np.radians(call.theta)
         if call.theta != 0:
            call.matriz_rotacao(thetaRadianos)
         
-        matriz_final = np.matmul(prd, call.rotacao)  
+        matriz_final = np.matmul(transformacao, call.rotacao)  
         pT = np.matmul(matriz_final, po)         
         
         print("theta em radianos: ", thetaRadianos)
@@ -46,10 +41,10 @@ while not call.end_loop:
         print("centro do eixo y: ", call.centroY)
         
         plt.xlim((-xMaior*4, xMaior*4)), plt.ylim((-yMaior*4, yMaior*4)) 
-        xlist = np.append(pT[0, :], pT[0, 0])
-        ylist = np.append(pT[1, :], pT[1, 0])
-        plt.plot(xlist, ylist, '-r',)
-        plt.fill_between(xlist, ylist) 
+        listaX = np.append(pT[0, :], pT[0, 0])
+        listaY = np.append(pT[1, :], pT[1, 0])
+        plt.plot(listaX, listaY, '-r',)
+        plt.fill_between(listaX, listaY) 
         plt.show()
         plt.pause(0.01)
         plt.clf()
