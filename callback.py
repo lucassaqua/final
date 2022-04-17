cf0a1a841007277e05e27d83cab8cb728cab718d
lucas <lucassaqua@live.com>
2022-04-17 17:06:01 -0300

    first commit

---
+++ callback.py
@@ -0,0 +1,60 @@
+# Exemplos de call-backs do Matplotlib para a interação com o usuário
+# Para terminar o programa, pressione a tecla 'escape'
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backend_bases import MouseButton
+
+centro_eixo_x = 0
+centro_eixo_y = 0
+escala = 1
+rotacao =np.identity(3,dtype=float)
+theta = 0
+
+end_loop = False
+
+# Função que gerencia eventos do mouse
+def on_press(event):
+    print('Você pressionou o botão do mouse:', event.button, event.xdata, event.ydata)
+    global centro_eixo_x
+    global centro_eixo_y
+    
+    centro_eixo_x = round(event.xdata,2)
+    centro_eixo_y = round(event.ydata,2)   
+
+# Função que gerencia eventos do teclado
+def on_key(event):
+    global end_loop
+    global escala
+    global theta
+    
+    print('Você pressionou a tecla: "', event.key, '"', event.xdata, event.ydata)
+    if event.key == 'escape': 
+        end_loop = True
+    elif event.key == 'up':
+        escala += 0.5
+    elif event.key == 'down':
+        escala -= 0.5
+    elif event.key == 'left':
+        theta += 2
+        matriz_rotacao(0)
+    elif event.key == 'right':
+        theta -= 2
+        matriz_rotacao(0)
+    
+def matriz_rotacao(theta_rad):
+    global rotacao      
+    rotacao =np.matmul( np.array([ [ np.cos(theta_rad) , -np.sin(theta_rad),0]
+                                    ,[  np.sin(theta_rad) , np.cos(theta_rad),0]
+                                    ,[0,0,1]]) ,rotacao)
+
+
+def matriz_escala(escala):
+    return np.array([[escala, 0, 0],
+                     [0, escala, 0],
+                     [0, 0, 1]])
+
+def matriz_translacao(centro_eixo_x, centro_eixo_y):
+    return np.array([[1, 0, centro_eixo_x],
+                     [0, 1, centro_eixo_y],
+                     [0, 0, 1]])
---
+++ trabalho_2.py
@@ -0,0 +1,60 @@
+#%matplotlib qt
+import callback as call  
+import numpy as np
+import matplotlib.pyplot as plt
+import matrizes as mt
+
+
+
+matriz = np.array([[2,2,1], [4,2,1], [3,4,1]])
+# matriz = np.array([[-0.5,-0.5,1],[0.5,-0.5,1],[0.5,0.5,1],[-0.5,0.5,1]])
+po = np.transpose(matriz)
+
+xmin = np.amin(po[0,:])
+xmax = np.amax(po[0,:])
+
+ymin = np.amin(po[1,:])
+ymax = np.amax(po[1,:])
+
+centro_eixo_x = 0.
+centro_eixo_y = 0.
+
+           
+fig, ax = plt.subplots()
+
+cid = fig.canvas.mpl_connect('button_press_event', call.on_press)
+cid = fig.canvas.mpl_connect('key_press_event', call.on_key)
+
+
+while not call.end_loop:             
+        if call.end_loop == True:
+            break
+
+            
+        prd = np.matmul( call.matriz_translacao(call.centro_eixo_x, call.centro_eixo_y),call.matriz_escala(call.escala))
+        
+        theta_rad=np.radians(call.theta)
+        if call.theta != 0:
+           call.matriz_rotacao(theta_rad)
+        
+        matriz_final = np.matmul(prd, call.rotacao)  
+        pT = np.matmul(matriz_final,po)         
+        
+        
+        # plt.title("theta : ",theta)
+        print("theta_rad : ",theta_rad)
+        print("velocidade_rotacao : ",call.rotacao)
+        print("escala",call.escala)
+        print("centro_eixo_x",call.centro_eixo_x)
+        print("centro_eixo_y",call.centro_eixo_y)
+        
+        
+        plt.xlim((-xmax*4, xmax*4)), plt.ylim((-ymax*4, ymax*4)) 
+        xlist = np.append(pT[0, :], pT[0, 0])
+        ylist = np.append(pT[1, :], pT[1, 0])
+        plt.plot(xlist, ylist, '-r',)
+        plt.fill_between(xlist,ylist) 
+        plt.show()
+        plt.pause(0.01)
+        plt.clf()
+        
